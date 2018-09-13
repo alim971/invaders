78a66c553fc9ced995199567e0d08e6a9a084b68
Jozef Bugoš <bugosjoz@fel.cvut.cz>
2018-09-13 01:11:44 -0500

    finished invaders game

---
+++ js/script.js
@@ -0,0 +1,473 @@
+$(document).ready(function(){
+    var main = new Main();
+    main.initGame();
+    main.run();
+});
+
+let updateLogic = false;
+let direction = 1;
+let enemySpeed = 6;
+let enemyDownMove = 0;
+
+var invader = new Image();
+invader.src = "images/alien.png";
+
+
+var ship = new Image();
+ship.src = "images/ship1.png";
+
+var bullet = new Image();
+bullet.src = "images/eprojectile.png";
+
+class Sprite {
+        get height() {
+            return this._height;
+        }
+
+        set height(value) {
+            this._height = value;
+        }
+        get width() {
+            return this._width;
+        }
+
+        set width(value) {
+            this._width = value;
+        }
+        get speed() {
+            return this._speed;
+        }
+
+        set speed(value) {
+            this._speed = value;
+        }
+        get id() {
+            return this._id;
+        }
+
+        set id(value) {
+            this._id = value;
+        }
+        get y() {
+            return this._y;
+        }
+
+        set y(value) {
+            this._y = value;
+        }
+        get x() {
+            return this._x;
+        }
+
+        set x(value) {
+            this._x = value;
+        }
+        constructor(x,y,id,img, width, height,speed,canvas) {
+            this._x = x;
+            this._y = y;
+            this._id = id;
+
+            this.img = img;
+            this._speed = speed;
+            this._width = width;
+            this._height = height;
+            this.canvas = canvas;
+        }
+
+        update(timeDelta, input) {
+        }
+    }
+
+    class PlayerSprite extends Sprite{
+        get shouldFire() {
+            return this._shouldFire;
+        }
+
+        set shouldFire(value) {
+            this._shouldFire = value;
+        }
+        get lives() {
+            return this._lives;
+        }
+        get score() {
+            return this._score;
+        }
+
+        set score(value) {
+            this._score = value;
+        }
+        constructor(x,y,id,canvas) {
+            super(x,y,id,ship,30,16,5,canvas);
+            this._score = 0;
+            this._lives = 3;
+            this._shouldFire = false;
+        }
+
+        update(timeDelta,input) {
+            if (input.isDown(39)){
+                if (this.x + (this._width + 6) <= this.canvas.width){
+                    this.x += this._speed;
+                }
+            }
+
+            if (input.isDown(37)){
+                if (this.x - 6 >= 0){
+                    this.x -= this._speed;
+                }
+            }
+
+            if (input.isDown(32)) {
+                this._shouldFire = true;
+                delete input.down[32];
+            }
+        }
+    }
+
+    class EnemySprite extends Sprite{
+        get time() {
+            return this._time;
+        }
+
+        set time(value) {
+            this._time = value;
+        }
+        get shouldFire() {
+            return this._shouldFire;
+        }
+
+        set shouldFire(value) {
+            this._shouldFire = value;
+        }
+        constructor(x,y,id,canvas) {
+            super(x,y,id,invader,20,16,6,canvas);
+            this.delay = 720;
+            this._time = 0;
+            this._shouldFire = false;
+            this.down = 0;
+            this.direction = 1;
+        }
+
+        update(timeDelta) {
+            this._time += timeDelta;
+
+            if (this._time >= this.delay){
+
+                // Randomly shoot, based on delay so the the faster they get the more they fire.
+                var fireTest = Math.floor(Math.random() * (this.delay + 1));
+
+                if (fireTest < (this.delay / 200)){
+                    this._shouldFire = true;
+                }
+
+                if (this.x + (this._width + 6) >= this.canvas.width && direction === 1){
+                    updateLogic = true;
+                }
+                else if(this.x - 6 <= 0 && direction !== 1){
+                    updateLogic = true;
+                }
+
+                this.x += enemySpeed;
+                if(this.down === 3) {
+                    this.direction *= (-1);
+                    this.down = -1;
+                } else
+                    this.y += (enemySpeed / 2)* this.direction;
+                this.down++;
+                this._time = 0;
+            }
+
+            this.y += enemyDownMove;
+        };
+    }
+
+    class BulletSprite extends Sprite{
+        constructor(x,y,id,speed,canvas) {
+            super(x,y,id,bullet,30,0,speed,canvas);
+            this.delay = 500;
+            this.time = 0;
+        }
+
+        update(timeDelta) {
+            this.time += timeDelta;
+            this.y -= this._speed;
+            if (this.time >= this.delay){
+                this.time = 0;
+            }
+        }
+    }
+
+    class Renderer {
+        get player() {
+            return this._player;
+        }
+
+        set player(value) {
+            this._player = value;
+        }
+        constructor(canvas) {
+            this._enemies  = {};
+            this._player = null;
+            this._projectiles = {};
+            this._canvas    = canvas;
+            this._context   = this._canvas.getContext("2d");
+            this._width     = this._canvas.width;
+            this._height    = this._canvas.height;
+        }
+
+        redraw()
+        {
+            // foreground
+            this._context.clearRect(0, 0,this._width, this._height);
+            this._context.fillStyle = 'black';
+            for (var id in this._enemies) {
+                var enemy = this._enemies[id];
+                this._context.save();
+                this._context.drawImage(enemy.img, enemy.x, enemy.y);
+                this._context.restore();
+            }
+
+                this._context.save();
+                this._context.drawImage(this.player.img, this.player.x, this.player.y);
+
+                for (let i=0; i<= this.player._lives; i++){
+                    this._context.drawImage(this.player.img, (this._width / 2) + 180 + (i * 38), 10);
+                }
+
+                this._context.restore();
+
+            for (var id in this._projectiles) {
+                var projectile = this._projectiles[id];
+                this._context.save();
+                this._context.drawImage(projectile.img, projectile.x, projectile.y);
+                this._context.restore();
+            }
+        }
+
+        addEnemy(enemy)
+        {
+            this._enemies[enemy.id] = enemy;
+        }
+
+        removeEnemy(enemy)
+        {
+            delete this._enemies[enemy.id];
+        }
+
+
+        addBullet(projectile)
+        {
+            this._projectiles[projectile.id] = projectile;
+        }
+
+        removeBullet(projectile)
+        {
+            delete this._projectiles[projectile.id];
+        }
+    }
+
+class InputHandeler {
+        get down() {
+            return this._down;
+        }
+
+        set down(value) {
+            this._down = value;
+        }
+        constructor() {
+            this._down = {};
+            var _this = this;
+            // capture key presses
+            document.addEventListener("keydown", function(evt) {
+                if(evt.keyCode <= 40)
+                    evt.preventDefault();
+                _this._down[evt.keyCode] = true;
+            });
+            document.addEventListener("keyup", function(evt) {
+                if(evt.keyCode <= 40)
+                    evt.preventDefault();
+                delete  _this._down[evt.keyCode];
+            });
+        }
+
+        isDown(keyCode) {
+            return this._down[keyCode];
+        }
+    }
+
+    class Main {
+        get player() {
+            return this._player;
+        }
+
+        set player(value) {
+            this._player = value;
+        }
+        constructor() {
+            this.canvas = document.getElementById("canvas");
+            this.canvasBuff = document.getElementById("canvas_buff")
+            this.inputHandeler = new InputHandeler()
+
+            this.renderer = new Renderer(this.canvasBuff);
+
+            this.gameOver = false;
+            this.wave = -1;
+
+            this.enemyCount = 0;
+            this.enemies = {};
+
+            this._player = null;
+
+            this.projectileCount = 0;
+            this.projectiles = {};
+            this.initGame();
+        }
+
+        addEnemy(enemy) {
+            this.enemies[enemy.id] = enemy;
+            this.renderer.addEnemy(enemy);
+            ++this.enemyCount;
+        }
+
+        removeEnemy(enemy) {
+            delete this.enemies[enemy.id];
+            this.renderer.removeEnemy(enemy);
+            --this.enemyCount;
+
+            if (this.enemyCount === 0) {
+                this.nextWave();
+            }
+        }
+
+        addBullet(projectile) {
+            this.projectiles[projectile.id] = projectile;
+            this.renderer.addBullet(projectile);
+            ++this.projectileCount;
+        }
+
+        removeBullet(projectile) {
+            delete this.projectiles[projectile.id];
+            this.renderer.removeBullet(projectile);
+        }
+
+// Set up the initial game
+        initGame() {
+            this._player = new PlayerSprite(10, this.canvas.height - 30, 0, this.canvas);
+            this.renderer._player = this._player;
+
+            this.nextWave();
+        }
+
+// Reset the enemies for the next wave
+        nextWave() {
+            this.wave++;
+            for (let i = 0; i <= 11; i++) {
+                for (let j = 0; j <= 6; j++) {
+                    var enemy = new EnemySprite(10 + (i * 34), 40 + (j * 25), this.enemyCount, this.canvas);
+                    this.addEnemy(enemy);
+                }
+            }
+        }
+
+        run() {
+            var _this = this;
+            var loop = function () {
+                _this.update();
+                _this.renderer.redraw();
+                _this.canvas.getContext("2d").clearRect(0, 0, _this.canvasBuff.width, _this.canvasBuff.height);
+                _this.canvas.getContext("2d").drawImage(_this.canvasBuff, 0, 0);
+                window.requestAnimationFrame(loop, _this.canvas);
+            };
+            window.requestAnimationFrame(loop, this.canvas);
+        }
+
+        update() {
+            if (this.gameOver !== true) {
+                if (this._player._lives < 0 || this.enemyPassed()) {
+                    this.gameOver = true;
+                    $('#gameover').fadeIn(2000);
+                }
+
+                this._player.update(100, this.inputHandeler);
+                if (this._player.shouldFire === true) {
+                    this._player.shouldFire = false;
+                    var projectile = new BulletSprite(this._player.x + (this._player._width / 2) - 6, this._player.y - 10, this.projectileCount, 5, this.canvas);
+                    this.addBullet(projectile);
+                }
+                $('#player_score').html(this._player._score);
+            }
+
+            for (var id in this.projectiles) {
+                var projectile = this.projectiles[id];
+                projectile.update(100, this.inputHandeler);
+            }
+
+            // Change the direction of the inaders movement
+            if (updateLogic === true) {
+                updateLogic = false;
+                direction *= -1;
+                enemySpeed *= -1;
+                enemyDownMove = 10;
+            }
+
+            // Loop through the enemies update their speed baased on enemies left, check if they have fired.
+            for (var id in this.enemies) {
+                var enemy = this.enemies[id];
+                enemy.delay = (this.enemyCount * 20) - (this.wave * 10);
+
+                if (enemy.delay <= 80) {
+                    enemy.delay = 80;
+                }
+
+                enemy.update(100, this.inputHandeler);
+
+                if (enemy.shouldFire === true) {
+                    enemy.shouldFire = false;
+                    var projectile = new BulletSprite(enemy.x + (enemy._width / 2), enemy.y, this.projectileCount, -5, this.canvas);
+                    this.addBullet(projectile);
+                }
+            }
+
+            // reset the y var for the enemies to move down.
+            enemyDownMove = 0;
+
+            // Check collisions between player, enemies, and projectiles
+            this.checkCollisions();
+        }
+
+        checkCollisions() {
+            for (var id in this.projectiles) {
+                var projectile = this.projectiles[id];
+
+                if (projectile.speed > 0) {
+                    for (var eid in this.enemies) {
+                        var enemy = this.enemies[eid];
+
+                        if (this.isCollision(projectile,enemy)) {
+                            this.removeEnemy(enemy);
+                            this.removeBullet(projectile);
+                            this.player._score += 100;
+                        }
+                    }
+                } else if(this.isCollision(projectile,this.player)) {
+                            this.removeBullet(projectile);
+                            this.player._lives--;
+                        }
+                if (projectile.y <= 0 || projectile.y > this.canvas.height) {
+                    this.removeBullet(projectile);
+                }
+            }
+        }
+
+        isCollision(sprite1,sprite2) {
+            return sprite1.x >= sprite2.x && sprite1.x <= (sprite2.x + sprite2.width) && sprite1.y <= (sprite2.y + sprite2.height) && sprite1.y >= sprite2.y;
+        }
+
+        enemyPassed() {
+            for (var eid in this.enemies) {
+                var enemy = this.enemies[eid];
+                if (enemy.y >= this._player.y)
+                    return true;
+            }
+            return false;
+        }
+    }
